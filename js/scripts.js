f1951b46b8e6c26cf780e533ab613858baa9bddd
Arisaymella <elgrankeko@hotmail.com>
2024-04-08 18:54:10 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -99,10 +99,86 @@ function measures(kilometros) {
 const finalMeasures = measures(50);
 console.log(finalMeasures);
 
-function hours (horas){
+function hours(horas) {
   const finalHours = horas * 60;
   return finalHours;
 }
- const finalHours = hours(126)
+const finalHours = hours(126);
 
- console.log(finalHours + ' segundos')
+console.log(finalHours + ' segundos');
+
+function nameAndAge(name, age) {
+  if (age >= 18) {
+    console.log('Hola ' + name + ' eres mayor de edad.');
+  } else if (age <= 18) {
+    console.log('Hola ' + name + ' eres menor de edad.');
+  }
+}
+nameAndAge(`pedro`, 25);
+
+function twoNumbers(oneNumber, twoNumber) {
+  if (oneNumber > twoNumber) {
+    console.log(oneNumber + ' es mayor.');
+  } else {
+    console.log(twoNumber + ' es mayor');
+  }
+}
+twoNumbers(36, 55);
+
+function numberPositive(numberPositive) {
+  if (numberPositive > 0) {
+    console.log(numberPositive + ' positivo');
+  } else if (numberPositive < 0) {
+    console.log(numberPositive + ' es negativo');
+  } else if (numberPositive === 0) {
+    console.log(numberPositive + ' es 0');
+  }
+}
+
+numberPositive(56);
+
+function notes(noteOne, noteTwo, noteTrhee) {
+  const media = (noteOne + noteTwo + noteTrhee) / 3;
+
+  if (media < 5) {
+    console.log(media + ' es suspenso');
+  } else if (media > 5) {
+    console.log(media + ' es aprobado');
+  } else if (media > 8) {
+    console.log(media + ' es matricula de honor');
+  }
+}
+
+notes(2, 7, 9);
+
+function threeNumers(numberOne, numberTwo, numberThree) {
+  if (numberOne > numberTwo && numberOne > numberThree) {
+    console.log(numberOne + ' es el mas mayor');
+  } else if (numberTwo > numberOne && numberTwo > numberThree) {
+    console.log(numberTwo + ' es el mas mayor');
+  } else if (numberThree > numberOne && numberThree > numberTwo) {
+    console.log(numberThree + ' es el mas mayor');
+  }
+}
+
+threeNumers(20, 34, 33);
+
+function division(number) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    console.log(number + ' es divisible por 3 y 5');
+  } else if (number % 5 === 0) {
+    console.log(number + ' es divisible por 5');
+  } else if (number % 3 === 0) {
+    console.log(number + ' es divisible por 3');
+  }
+}
+division(20);
+
+function numberWhole(number) {
+  if (number % 2 === 0) {
+    console.log(number + ' es par');
+  } else {
+    console.log(number + ' es impar');
+  }
+}
+numberWhole(26);
